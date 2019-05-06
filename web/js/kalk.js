769323af7b76a9c51ab7b228f35c0327eea2ddfb
dadic333 <hans333@seznam.cz>
2019-05-06 21:25:08 +0200

    14. vložení

---
+++ web/js/kalk.js
@@ -0,0 +1,13 @@
+  window.onload = function() {
+
+    let button = document.getElementById("tlacitko");
+    let cislo1 = document.getElementById("cislo1");
+    let cislo2 = document.getElementById("cislo2");
+
+    function secti(){
+      alert(parseInt(cislo1.value) + parseInt(cislo2.value));
+    }
+
+    button.onclick = secti;
+  };
+
